6dfd8ee830218c45bd6dc5c718d7d7bd00875cd0
codeawy <codeawy@gmail.com>
2026-05-13 11:49:01 +0300

    Apply repository design pattern

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
     return {
       status: 'success',
@@ -13,11 +21,11 @@ export class ProductsService {
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    return this.productRepository.findOne({ where: { id } });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
